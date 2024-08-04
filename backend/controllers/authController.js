5ddf467d0ee20a13ea437c5e2705c7d3ddb57a07
Arfin <94512463+Arfin6900@users.noreply.github.com>
2024-08-04 14:39:26 +0500

    fixes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,55 +1,55 @@
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+      const User = require("../models/User");
+      const jwt = require("jsonwebtoken");
+      const bcrypt = require("bcryptjs");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
+      const generateToken = (id) => {
+        return jwt.sign({ id }, "hackathon", {
+          expiresIn: "30d",
+        });
+      };
 
-exports.registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+      exports.registerUser = async (req, res) => {
+        const { name, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+        const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    return res.status(400).json({ message: "User already exists" });
-  }
+        if (userExists) {
+          return res.status(400).json({ message: "User already exists" });
+        }
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+        const user = await User.create({
+          name,
+          email,
+          password,
+        });
 
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400).json({ message: "Invalid user data" });
-  }
-};
+        if (user) {
+          res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+          });
+        } else {
+          res.status(400).json({ message: "Invalid user data" });
+        }
+      };
 
-exports.authUser = async (req, res) => {
-  const { email, password } = req.body;
+      exports.authUser = async (req, res) => {
+        const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401).json({ message: "Invalid email or password" });
-  }
-};
+        if (user && (await user.matchPassword(password))) {
+          res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: generateToken(user._id),
+          });
+        } else {
+          res.status(401).json({ message: "Invalid email or password" });
+        }
+      };
